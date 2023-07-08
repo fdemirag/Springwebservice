a3dfefc25ccf6e1a26bae470906e94bf028f6acc
fatihdemirag <f2000fatih@hotmail.com>
2023-07-08 03:05:41 +0300

    UserController'a username validasyonu eklendi.

--- src/main/java/com/hoaxify/webservice/user/UserController.java
+++ src/main/java/com/hoaxify/webservice/user/UserController.java
@@ -1,11 +1,17 @@
 package com.hoaxify.webservice.user;
 
+import com.hoaxify.webservice.error.ApiError;
 import com.hoaxify.webservice.shared.GenericResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 
@@ -19,9 +25,17 @@ public class UserController {
 
     //@CrossOrigin proxy kullandığımız için gerek kalmadı
     @PostMapping("/api/1.0/users")
-    public GenericResponse save(@RequestBody User user){
+    public ResponseEntity<?> save(@RequestBody User user){
+        String username= user.getUsername();
+        if (username ==null || username.isEmpty()) {
+            ApiError error = new ApiError(400,"validation error ", "/api/1.0/users");
+            Map<String, String> validationErrors = new HashMap<>();
+            validationErrors.put("username","username cannot be null!");
+            error.setValidationErrors(validationErrors);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+        }
         userService.save(user);
-        return new GenericResponse("User Created");
+        return  ResponseEntity.ok(new GenericResponse("User Created"));
 
     }
 }
