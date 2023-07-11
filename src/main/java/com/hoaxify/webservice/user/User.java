ed99006139cc34b0f4a7175817f83de73d356c27
fatihdemirag <f2000fatih@hotmail.com>
2023-07-11 03:21:58 +0300

    @NotNull @NotBlank ile değiştirildi. @Size ile min-max karakter belirlendi. password'e @Pattern eklendi.

--- src/main/java/com/hoaxify/webservice/user/User.java
+++ src/main/java/com/hoaxify/webservice/user/User.java
@@ -2,9 +2,12 @@ package com.hoaxify.webservice.user;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
-import org.antlr.v4.runtime.misc.NotNull;
+
 
 @Data
 @Entity
@@ -15,11 +18,16 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @NotNull
+    @NotBlank
+    @Size(min=4,max=255)
     private String username;
 
-    @NotNull
+    @NotBlank
+    @Size(min=4,max=255)
     private String displayName;
 
+    @NotBlank
+    @Size(min=8,max=255)
+    @Pattern(regexp="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$")
     private String password;
 }
