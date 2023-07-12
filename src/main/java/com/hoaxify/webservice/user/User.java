00d7c10620813585bca2a2f56d237a844fd9639d
fatihdemirag <f2000fatih@hotmail.com>
2023-07-12 07:33:33 +0300

    Dil desteğine uygun validasyon mesajları için ekleme yapıldı.

--- src/main/java/com/hoaxify/webservice/user/User.java
+++ src/main/java/com/hoaxify/webservice/user/User.java
@@ -18,7 +18,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @NotBlank
+    @NotBlank(message ="{hoaxify.constraint.username.NotNull.message}")
     @Size(min=4,max=255)
     @UniqueUsername
     private String username;
@@ -29,6 +29,6 @@ public class User {
 
     @NotBlank
     @Size(min=8,max=255)
-    @Pattern(regexp="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$")
+    @Pattern(regexp="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$", message="{hoaxify.constrain.password.Pattern.message}")
     private String password;
 }
